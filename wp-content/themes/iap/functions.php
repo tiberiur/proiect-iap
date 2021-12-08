0cf84fc6cff3314ae8dec994243997d5c83e8853
Tiberiu Rus <ro.tiberiu@gmail.com>
2021-12-08 17:22:42 +0200

    refine: transform css -> scss + build tools

--- wp-content/themes/iap/functions.php
+++ wp-content/themes/iap/functions.php
@@ -6,6 +6,7 @@
  */
 function iap_scripts() {
     wp_enqueue_style( 'style', untrailingslashit( get_template_directory_uri() ) . '/style.css' );
+    wp_enqueue_style( 'style-scss', untrailingslashit( get_template_directory_uri() ) . '/dist/css/style.css' );
     wp_enqueue_style( 'font-google', 'https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap', false ); 
 }
 add_action( 'wp_enqueue_scripts', 'iap_scripts' );
